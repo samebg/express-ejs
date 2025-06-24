39e21c2d5fe3a19976d6d0dc591fdfea88cfbe1e
samebg <samebengho@gmail.com>
2025-06-24 01:06:01 -0700

    Maybe final push

--- models/mongoose.js
+++ models/mongoose.js
@@ -1,7 +1,7 @@
 // models/mongoose.js
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/funnyrecipes';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/funny_recipes';
 
 mongoose.connect(MONGO_URI, {
   serverApi: { version: '1', strict: true, deprecationErrors: true }
